6cc8c54763315c0a049c5d6ed31ae7566621f448
Long Hoang <long88hoang@gmail.com>
2022-11-30 12:33:21 -0600

    Tip Calculator express application created.

--- server.js
+++ server.js
@@ -9,9 +9,16 @@ let port = 3000
 
 // Mount routes
 app.get('/greeting/:name', (req, res) => {
-	console.log(req.params);
+	console.log(req.params)
 	res.send(`Hey! How ya been, ` + req.params.name + '?');
-});
+})
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+	console.log(req.params)
+	let result = (req.params.total * (req.params.tipPercentage / 100))
+	res.send(`${result}`)
+})
+
 
 //Tell the app to listen to port 3000
 app.listen(port, function () {
