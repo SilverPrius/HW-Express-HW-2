be324634fc857815c3defcf60c0031271dd2138c
Long Hoang <long88hoang@gmail.com>
2022-11-30 12:57:46 -0600

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 
+
+app.get('/magic/:question', (req, res) => {
+	console.log(req.params)
+
+	const myArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+	res.send(myArray[Math.floor(Math.random()*myArray.length)])
+})
+
+
 //Tell the app to listen to port 3000
 app.listen(port, function () {
     console.log(`Listening to port ${port}` )
