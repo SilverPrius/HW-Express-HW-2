a7120fa7eeaf55e9167176e827f93b5c22370283
Long Hoang <long88hoang@gmail.com>
2022-11-30 18:15:48 -0600

    Added <h1> tag to 8 ball answer

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ let port = 3000
 // Mount routes
 app.get('/greeting/:name', (req, res) => {
 	console.log(req.params)
-	res.send(`Hey! How ya been, ` + req.params.name + '?');
+	res.send(`Hey! How ya been, ` + req.params.name + '?')
 })
 
 app.get('/tip/:total/:tipPercentage', (req, res) => {
@@ -19,18 +19,17 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 	res.send(`${result}`)
 })
 
-
-
 app.get('/magic/:question', (req, res) => {
 	console.log(req.params)
 
-	const myArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+	const myArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
 
-	res.send(myArray[Math.floor(Math.random()*myArray.length)])
+	let answer = myArray[Math.floor(Math.random() * myArray.length)]
+	res.send(`<h1>${answer}</h1>`)
 })
 
 
 //Tell the app to listen to port 3000
 app.listen(port, function () {
-    console.log(`Listening to port ${port}` )
+	console.log(`Listening to port ${port}`)
 })
